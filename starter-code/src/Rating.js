7605cd37cf293b69c7c2ac48001cbc78a47730b3
Victoria Kiseleva <111victoria.toria@gmail.com>
2020-05-06 09:58:59 +0200

    rating

---
+++ starter-code/src/Rating.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Rating extends Component {
+
+  printStars = (num) => {
+    let stars = Math.round(num.children);
+    switch (stars) {
+      case 0:
+      return stars = '☆☆☆☆☆';
+      case 1:
+        return stars = '★☆☆☆☆';
+      case 2:
+        return stars = '★★☆☆☆';
+      case 3:
+        return stars = '★★★☆☆';
+      case 4:
+        return stars = '★★★★☆';
+      case 5:
+        return stars = '★★★★★';
+
+      default:
+        return stars= 'Not rated';
+    }
+  }
+
+  render(){
+    const { children } = this.props;
+    return (
+      <div style= {{'fontSize':'40px'}}>{this.printStars({children})}</div>
+    )
+  }
+}
+
+export default Rating; 
